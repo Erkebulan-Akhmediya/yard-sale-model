2572d3c26cc32884328b0d45730ac3effef432a2
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-06-21 15:54:19 +0500

    reading any line

--- viz.c
+++ viz.c
@@ -46,6 +46,48 @@ int count_data(float *max, int *row_count, int *col_count) {
     return 0;
 }
 
+void skip_lines(FILE *f, int nlines) {
+    if (nlines == 0)
+        return;
+    
+    int new_line_count = 0;
+    int ch;
+    while ((ch = fgetc(f)) != EOF) {
+        if (ch == '\n')
+            new_line_count++;
+        if (nlines == new_line_count)
+            break;
+    }
+}
+
+float *read_line(int row_index, int row_len) {
+    FILE *f = fopen("ysm.csv", "r");
+    if (f == NULL)
+        return NULL;
+    
+    skip_lines(f, row_index);
+    float *line = calloc(row_len, sizeof(float));
+    int next_free_index = 0;
+    int ch;
+    DStr *num_dstr = new_dstr();
+    while ((ch = fgetc(f)) != EOF) {
+        if (ch != ',' && ch != '\n') {
+            dstr_append(num_dstr, ch);
+            continue;
+        }
+        char *num_str = dstr_to_str(num_dstr);
+        float num = (float) atof(num_str);
+        line[next_free_index++] = num;
+        dstr_clear(num_dstr);
+
+        if (ch == '\n')
+            break;
+    }
+    dstr_free(num_dstr);
+    fclose(f);
+    return line;
+}
+
 int main() {
 
     float max;
@@ -55,6 +97,20 @@ int main() {
         return -1;
     }
     printf("max: %f\nrow count: %d\ncol count: %d\n", max, row_count, col_count);
+
+    // read and print first 3 rows (for testing)
+    for (int row_index = 0; row_index < 3; row_index++) {
+        printf("row %d: ", row_index);
+        float *line = read_line(row_index, col_count);
+        if (line == NULL) {
+            perror("failed to read a line");
+            continue;
+        }
+        for (int i = 0; i < col_count; i++)
+            printf("%.2f ", line[i]);
+        printf("\n");
+        free(line);
+    }
 
     InitWindow(WIN_wIDTH, WIN_HEIGHT, WIN_TITLE);
 
