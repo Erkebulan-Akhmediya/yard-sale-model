0f6fb02c9a7bf9f29906b8f4f25694c70356f318
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-06-21 12:21:00 +0500

    fixed csv formatting

--- ysm.c
+++ ysm.c
@@ -30,8 +30,11 @@ void exchange(float *arr, int arr_size) {
 }
 
 void write_to_csv(FILE *file, float *arr, int arr_size) {
-    for (int i = 0; i < arr_size; i++)
-        fprintf(file, "%.2f, ", arr[i]);
+    for (int i = 0; i < arr_size; i++) {
+        fprintf(file, "%.2f", arr[i]);
+        if (i < arr_size - 1)
+            fprintf(file, ",");
+    }
     fprintf(file, "\n");
 }
 
