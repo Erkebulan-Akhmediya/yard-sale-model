8cd8102955c217b2e1b57eadc7a0bbed00ae16a6
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-06-22 14:31:10 +0500

    drawing columns

--- README.md
+++ README.md
@@ -9,17 +9,7 @@ I did this for educational purposes out of pure curiosity.
 
 To compile:
 ```
-gcc -o ysm -O2 sim/main.c
-```
-
-To run executable on Linux:
-```
-./ysm
-```
-
-To run executable on Windows:
-```
-.\ysm.exe
+gcc -o sim -O2 sim/main.c
 ```
 
 #### Simulation Options
@@ -29,7 +19,7 @@ To run executable on Windows:
 
 To compile on Windows:
 ```
-gcc viz/main.c viz/dstr.c viz/dstr.h -o viz -I C:\raylib\w64devkit\include -L C:\raylib\w64devkit\lib -lraylib -lopengl32 -lgdi32 -lwinmm -O2
+gcc viz/main.c viz/dstr.c viz/dstr.h viz/csv.c viz/csv.h -o viz -I C:\raylib\w64devkit\include -L C:\raylib\w64devkit\lib -lraylib -lopengl32 -lgdi32 -lwinmm -O2
 ```
 
 #### Visualization Options
--- viz/main.c
+++ viz/main.c
@@ -5,16 +5,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define WIN_wIDTH 700
+#define WIN_WIDTH 700
 #define WIN_HEIGHT 500
 #define WIN_TITLE "Yard Sale Model"
 
 #define AXIS_WIDTH 3
 #define AXIS_PADDING 50
 
+#define COL_PAD 3
+#define COL_COLOR BLUE
+
+void draw_col(int x, int y, int width, int height) {
+    x += COL_PAD;
+    width -= COL_PAD;
+    DrawRectangle(x, y, width, height, COL_COLOR);
+}
+
 int main(int argc, char *argv[]) {
 
-    if (argc != 3 || argv[1] != "-f") {
+    if (argc != 3 || strcmp(argv[1], "-f") != 0) {
         perror("invalid arguments");
         return 1;
     }
@@ -25,23 +34,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("max: %f\nrow count: %d\ncol count: %d\n", csv->max, csv->row_count, csv->col_count);
-
-    // read and print first 3 rows (for testing)
-    for (int row_index = 0; row_index < 3; row_index++) {
-        printf("row %d: ", row_index);
-        float *line = csv_get_line(csv, row_index);
-        if (line == NULL) {
-            perror("failed to read a line");
-            continue;
-        }
-        for (int i = 0; i < csv->col_count; i++)
-            printf("%.2f ", line[i]);
-        printf("\n");
-        free(line);
-    }
-
-    InitWindow(WIN_wIDTH, WIN_HEIGHT, WIN_TITLE);
+    InitWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);
 
     if (!IsWindowReady()) {
         perror("Failed to init window");
@@ -52,11 +45,31 @@ int main(int argc, char *argv[]) {
         BeginDrawing();
         ClearBackground(WHITE);
 
+        int x_width = WIN_WIDTH - (2 * AXIS_PADDING);
+        int y_height = WIN_HEIGHT - (2 * AXIS_PADDING);
+        int col_width = x_width / csv->col_count;
+
         // draw x-axis
-        DrawRectangle(AXIS_PADDING, WIN_HEIGHT - AXIS_PADDING, WIN_wIDTH - (2 * AXIS_PADDING), AXIS_WIDTH, GRAY);
+        DrawRectangle(AXIS_PADDING, WIN_HEIGHT - AXIS_PADDING, x_width, AXIS_WIDTH, GRAY);
 
         // draw y-axis
-        DrawRectangle(AXIS_PADDING, AXIS_PADDING, AXIS_WIDTH, WIN_HEIGHT - (2 * AXIS_PADDING), GRAY);
+        DrawRectangle(AXIS_PADDING, AXIS_PADDING, AXIS_WIDTH, y_height, GRAY);
+
+        int init_col_x = AXIS_PADDING + AXIS_WIDTH;
+        int init_col_y = WIN_HEIGHT - AXIS_PADDING - AXIS_WIDTH;
+
+        float *line = csv_get_line(csv, 0);
+        if (line == NULL) {
+            EndDrawing();
+            break;
+        }
+        for (int i = 0; i < csv->col_count; i++) {
+            int col_height = y_height * (line[i] / csv->max);
+            int col_x = init_col_x + i * col_width;
+            int col_y = init_col_y - col_height;
+            draw_col(col_x, col_y, col_width, col_height);
+        }
+        free(line);
 
         EndDrawing();
     }
