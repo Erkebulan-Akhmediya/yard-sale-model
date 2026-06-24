ff336da54ac7e79324660504e3f46dcea9cd4afa
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-06-24 09:37:12 +0500

    moved bst related files to right place

---
+++ sim/bst.c
@@ -0,0 +1,78 @@
+#include "bst.h"
+#include "stack.h"
+#include <stdlib.h>
+
+struct BSTNode {
+    float val;
+    struct BSTNode *left;
+    struct BSTNode *right;
+};
+
+void bst_init(BST *bst) {
+    bst->root = NULL;
+    bst->size = 0;
+}
+
+int _bst_insert(struct BSTNode *node, float val) {
+    if (val > node->val) {
+        if (node->left != NULL)
+            return _bst_insert(node->left, val);
+        node->left = malloc(sizeof(struct BSTNode));
+        if (node->left == NULL)
+            return -1;
+        node->left->val = val;
+        node->left->left = NULL;
+        node->left->right = NULL;
+    } else {
+        if (node->right != NULL)
+            return _bst_insert(node->right, val);
+        node->right = malloc(sizeof(struct BSTNode));
+        if (node->right == NULL)
+            return -1;
+        node->right->val = val;
+        node->right->left = NULL;
+        node->right->right = NULL;
+    }
+    return 0;
+}
+
+int bst_insert(BST *bst, float val) {
+    if (bst->root == NULL) {
+        bst->root = malloc(sizeof(struct BSTNode));
+        if (bst->root == NULL)
+            return -1;
+        bst->root->val = val;
+        bst->root->left = NULL;
+        bst->root->right = NULL;
+        bst->size++;
+        return 0;
+    }
+    if (_bst_insert(bst->root, val) == -1)
+        return -1;
+    bst->size++;
+    return 0;
+}
+
+int bst_delete(BST *bst, float val) {}
+
+void _bst_to_arr(Stack *stack, struct BSTNode *node) {
+    if (node->left != NULL)
+        _bst_to_arr(stack, node->left);
+    
+    stack_push(stack, node->val);
+
+    if (node->right != NULL)
+        _bst_to_arr(stack, node->right);
+}
+
+float *bst_to_arr(BST *bst) {
+    if (bst->root == NULL)
+        return NULL;
+
+    Stack stack;
+    if (stack_init(&stack, bst->size) == -1)
+        return NULL;
+
+    _bst_to_arr(&stack, bst->root);
+    return stack.arr;
+}
