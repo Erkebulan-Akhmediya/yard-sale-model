a83a02cda3577d29e7d87c6360fb9c003f42a983
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-07-02 11:20:29 +0500

    improved testing

--- sim/bst_test.c
+++ sim/bst_test.c
@@ -4,8 +4,10 @@
 
 void print_arr(float *arr, int size) {
     printf("bst.size = %d\n", size);
+    printf("[");
     for (int i = 0; i < size; i++)
         printf("%.2f ", arr[i]);
+    printf("]");
     printf("\n");
     printf("\n");
 }
@@ -25,67 +27,72 @@ void delete_and_print_arr(BST *bst, float val) {
 }
 
 void delete_root() {
-    printf("---------------------\n");
     printf("delete root test\n");
     BST bst;
     bst_init(&bst);
 
-    insert_and_print_arr(&bst, 67.0);
-
-    delete_and_print_arr(&bst, 67.0);
+    insert_and_print_arr(&bst, 67.0); // root
+    delete_and_print_arr(&bst, 67.0); // delete root
 
     bst_free(&bst);
     printf("---------------------\n");
 }
 
 void delete_leaf() {
-    printf("---------------------\n");
     printf("delete left test\n");
     BST bst;
     bst_init(&bst);
 
-    insert_and_print_arr(&bst, 67.0);
-
-    insert_and_print_arr(&bst, 69.0);
-
-    delete_and_print_arr(&bst, 69.0);
+    insert_and_print_arr(&bst, 67.0); // root
+    insert_and_print_arr(&bst, 69.0); // root->right
+    delete_and_print_arr(&bst, 69.0); // delete root->right
 
     bst_free(&bst);
     printf("---------------------\n");
 }
 
 void delete_node_with_both_children() {
-    printf("---------------------\n");
     printf("delete node with both children\n");
     BST bst;
     bst_init(&bst);
 
-    insert_and_print_arr(&bst, 42.0);
-    insert_and_print_arr(&bst, 67.0);
-    insert_and_print_arr(&bst, 52.0);
-    insert_and_print_arr(&bst, 69.0);
-    delete_and_print_arr(&bst, 67.0);
+    insert_and_print_arr(&bst, 42.0); // root
+    insert_and_print_arr(&bst, 67.0); // root->right
+    insert_and_print_arr(&bst, 52.0); // root->right->left
+    insert_and_print_arr(&bst, 69.0); // root->right->right
+    delete_and_print_arr(&bst, 67.0); // delete root->right
+
+    bst_free(&bst);
+    printf("---------------------\n");
+}
+
+void delete_node_with_right_child() {
+    printf("delete node with right child\n");
+    BST bst;
+    bst_init(&bst);
+
+    insert_and_print_arr(&bst, 52.0); // root
+    insert_and_print_arr(&bst, 67.0); // root->right
+    insert_and_print_arr(&bst, 69.0); // root->right->right
+    delete_and_print_arr(&bst, 67.0); // delete root->right
 
     bst_free(&bst);
     printf("---------------------\n");
 }
 
 void delete_node_with_left_child() {
-    printf("---------------------\n");
     printf("delete node with left child\n");
     BST bst;
     bst_init(&bst);
 
-    insert_and_print_arr(&bst, 52.0);
-    insert_and_print_arr(&bst, 67.0);
-    insert_and_print_arr(&bst, 69.0);
-    delete_and_print_arr(&bst, 67.0);
+    insert_and_print_arr(&bst, 52.0); // root
+    insert_and_print_arr(&bst, 67.0); // root->right
+    insert_and_print_arr(&bst, 61.0); // root->right->left
+    delete_and_print_arr(&bst, 67.0); // delete root->right
 
     bst_free(&bst);
     printf("---------------------\n");
 }
-
-void delete_node_with_right_child() {}
 
 int main() {
     delete_root();
